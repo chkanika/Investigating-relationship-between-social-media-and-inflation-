0c9f8d9ff0e228dda350934e11444d17add2cef1
Kanika Chouhan <kanika.chouhan890@gmail.com>
2023-07-11 23:05:48 +0530

    function

--- sample.py
+++ sample.py
@@ -25,5 +25,17 @@ def __init__(self):
         print("Error: Authentication Falied")
 
 def clean_tweet(self, tweet):
-    
-    
+    # utility function to clean yweet by removing links, special characters using simple regex statements. 
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def get_tweet_sentiment(self, tweet):
+    # utility function to classify sentiment of passed tweet using textblob's sentiment method 
+    # create textblob object of passed tweet text 
+    analysis = TextBlob(self.clean_tweet(tweet))
+    # set sentiment 
+    if analysis.sentiment.polarity > 0: 
+        return 'Positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
