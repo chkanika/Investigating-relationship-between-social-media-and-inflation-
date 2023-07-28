5829b4da5d31cb0dbec34eb31138203ee0f5adcf
Kanika Chouhan <kanika.chouhan890@gmail.com>
2023-07-28 11:56:43 +0530

    drafted

--- analysis.py
+++ analysis.py
@@ -1,1 +1,34 @@
+# mount drive 
+# set toolkit 
+
+
+
+
+
+# Importing essential libraries and functions
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+from numpy import array
+
+from tensorflow import keras
+from keras.preprocessing.text import one_hot, Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Activation, Dropout, Dense
+from keras.layers import Flatten, GlobalMaxPooling1D, Embedding, Conv1D, LSTM
+from sklearn.model_selection import train_test_split
+
+# importing the dataset 
+# tweets = 
+#  source? 
+
+# dataset exploration
+# tweets.shape  
+# tweets.head(5)
+
+import seaborn as sns
 
