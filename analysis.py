912d7c4abc46c9e5f68ef30824735cbe334d04f3
Kanika Chouhan <kanika.chouhan890@gmail.com>
2023-08-03 14:28:36 +0530

    analysis

--- analysis.py
+++ analysis.py
@@ -67,3 +67,49 @@
 # # sentences = list(tweets[""])
 # # for sen in sentences:
 #     # X.append(preprocess_text(sen))
+
+
+import tweepy
+from textblob import TextBlob
+
+# Set your Twitter API credentials
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+
+# Authenticate with Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Define the word you want to search for
+search_word = "YOUR_SEARCH_WORD"
+
+try:
+    # Fetch tweets containing the search word
+    tweets = api.search_tweets(q=search_word, count=100, lang="en", tweet_mode="extended")
+
+    # Process the fetched tweets and store sentiment along with the tweet text
+    analyzed_tweets = []
+    for tweet in tweets:
+        # Access tweet text
+        tweet_text = tweet.full_text
+        # Perform sentiment analysis using TextBlob
+        analysis = TextBlob(tweet_text)
+        # Determine sentiment polarity (ranging from -1 to 1, where < 0 is negative, > 0 is positive)
+        sentiment_polarity = analysis.sentiment.polarity
+
+        # Store the tweet along with its sentiment polarity
+        analyzed_tweets.append((tweet_text, sentiment_polarity))
+
+except tweepy.TweepError as e:
+    # Handle Twitter API errors here
+    print("Error occurred:", e)
+
+# Filter positive tweets
+ptweets = [tweet for tweet, sentiment in analyzed_tweets if sentiment > 0]
+
+# Process or store the positive tweets (ptweets) as per your research requirements
+for tweet_text in ptweets:
+    print(tweet_text)
--- sample.py
+++ sample.py
@@ -115,8 +115,8 @@ def main():
     for tweet in ntweets[:10]:
         print(tweet['text'])
         
-if __name__ == "__main__":
-    # calling main function 
-    main()
+# if __name__ == "__main__":
+#     # calling main function 
+#     main()
 
 
